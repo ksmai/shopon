22efa90401dc5496a720ee6aaefac97d301f2a46
ksmai <kasingmai@gmail.com>
2021-03-18 15:30:10 +0800

    Generate DefaultLayout with schematics for consistency

---
+++ apps/landing-page/components/shared/default-layout.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import DefaultLayout from './default-layout';
+
+describe('DefaultLayout', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<DefaultLayout />);
+    expect(baseElement).toBeTruthy();
+  });
+});
--- apps/landing-page/components/shared/default-layout.tsx
+++ apps/landing-page/components/shared/default-layout.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
-function Layout({ children }) {
+import styled from '@emotion/styled';
+
+/* eslint-disable-next-line */
+export interface DefaultLayoutProps {
+  children: React.ReactNode;
+}
+
+export function DefaultLayout({ children }: DefaultLayoutProps) {
   return (
-    <main>This is wrapped in Layout
+    <main>
+      <h1>Welcome to default-layout!</h1>
       {children}
     </main>
   );
 }
 
-export default Layout;
+export default DefaultLayout;
