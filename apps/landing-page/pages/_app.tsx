a5dfe50395ff87ac263e38c4d80659fe3c7c82f9
ksmai <kasingmai@gmail.com>
2021-03-18 14:51:31 +0800

    Remove auto-generated code

--- apps/landing-page/pages/_app.tsx
+++ apps/landing-page/pages/_app.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
-import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to landing-page!</title>
+        <title>Shopon</title>
       </Head>
-      <div className="app">
-        <header className="flex">
-          <NxLogo width="75" height="50" />
-          <h1>Welcome to landing-page!</h1>
-        </header>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </div>
+
+      <main className="app">
+        <Component {...pageProps} />
+      </main>
     </>
   );
 }
