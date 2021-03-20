8856864a1a1037b467e535c94b6e3cea0bb393a2
ksmai <kasingmai@gmail.com>
2021-03-20 21:58:08 +0800

    test: update tests for CustomApp

--- libs/homepage/custom-app/src/custom-app.spec.tsx
+++ libs/homepage/custom-app/src/custom-app.spec.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { AppProps } from 'next/app';
+import { render, screen } from '@testing-library/react';
 
 import CustomApp from './custom-app';
 
 describe('CustomApp', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<CustomApp />);
-    expect(baseElement).toBeTruthy();
+  interface ComponentProps {
+    propText: string;
+  }
+
+  let componentText: string;
+  let propText: string;
+  let Component: React.FunctionComponent<ComponentProps>;
+  let router: AppProps['router'];
+
+  beforeEach(() => {
+    componentText = 'Component Text';
+    propText = 'Prop Text';
+    Component = (props: ComponentProps) => (
+      <div>
+        <span>{componentText}</span>
+        <span>{props.propText}</span>
+      </div>
+    );
+    router = (jest.fn() as unknown) as AppProps['router'];
+    render(
+      <CustomApp
+        Component={(Component as unknown) as AppProps['Component']}
+        pageProps={{ propText }}
+        router={router}
+      />
+    );
+  });
+
+  it('should render Component', () => {
+    screen.getByText(componentText);
+  });
+
+  it('should pass pageProps to Component', () => {
+    screen.getByText(propText);
   });
 });
