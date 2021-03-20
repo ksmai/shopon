4a4374166598ec413f66c81e70926fc980d95eec
ksmai <kasingmai@gmail.com>
2021-03-20 12:38:08 +0800

    Config jest to work with next-optimized-image

--- apps/landing-page/jest.config.js
+++ apps/landing-page/jest.config.js
@@ -7,5 +7,7 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/landing-page',
-  setupFiles: ['./setup-test.js'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)(\\?.*)?$': "<rootDir>/__mocks__/fileMock.js",
+  },
 };
