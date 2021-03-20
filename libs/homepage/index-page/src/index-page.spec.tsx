13890ad51741250cbc97d740c218896e6cd5930a
ksmai <kasingmai@gmail.com>
2021-03-20 22:03:07 +0800

    refactor: move IndexPage to its lib

--- libs/homepage/index-page/src/index-page.spec.tsx
+++ libs/homepage/index-page/src/index-page.spec.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import Index from '../pages/index';
+import IndexPage from './index-page';
 
-describe('Index', () => {
+describe('IndexPage', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Index />);
+    const { baseElement } = render(<IndexPage />);
     expect(baseElement).toBeTruthy();
   });
 });
---
+++ libs/homepage/index-page/src/index-page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './index-page.module.scss';
+
+/* eslint-disable-next-line */
+export interface IndexPageProps {}
+
+export function IndexPage(props: IndexPageProps) {
+  return (
+    <div>
+      <h1>Welcome to IndexPage!</h1>
+    </div>
+  );
+}
+
+export default IndexPage;
