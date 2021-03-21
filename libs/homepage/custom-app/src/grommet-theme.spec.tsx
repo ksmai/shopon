7c105ab4c6960cc7e94074815e9d9403dc66a904
ksmai <kasingmai@gmail.com>
2021-03-21 12:03:32 +0800

    test: assert that GrommetTheme render children

--- libs/homepage/custom-app/src/grommet-theme.spec.tsx
+++ libs/homepage/custom-app/src/grommet-theme.spec.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import GrommetTheme from './grommet-theme';
 
 describe('GrommetTheme', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<GrommetTheme />);
-    expect(baseElement).toBeTruthy();
+  it('should render children', () => {
+    const children = 'This is a child text node';
+    render(<GrommetTheme>{children}</GrommetTheme>);
+    screen.getByText(children);
   });
 });
