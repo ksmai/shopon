08d92a4a7504836f98fc0e6f967d222a8cd84ff8
ksmai <kasingmai@gmail.com>
2021-03-27 11:01:07 +0800

    test: add getByTestId cypress command

--- apps/homepage-e2e/src/support/commands.ts
+++ apps/homepage-e2e/src/support/commands.ts
@@ -8,26 +8,17 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 
+type Options = Parameters<typeof cy.get>[1];
+
 // eslint-disable-next-line @typescript-eslint/no-namespace
 declare namespace Cypress {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface Chainable<Subject> {
-    login(email: string, password: string): void;
+    getByTestId(testId: string, options?: Options): Chainable<HTMLElement>;
   }
 }
-//
+
 // -- This is a parent command --
-Cypress.Commands.add('login', (email, password) => {
-  console.log('Custom command example: Login', email, password);
+Cypress.Commands.add('getByTestId', (testId: string, options?: Options) => {
+  return cy.get(`[data-testid=${testId}]`, options);
 });
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
