65524eb03529be80a86c993e79b998524ab9216f
ksmai <kasingmai@gmail.com>
2021-04-03 13:28:16 +0800

    chore: configure TypeOrmModule in AccountsApplicationModule

--- libs/accounts/application/src/lib/accounts-application.module.ts
+++ libs/accounts/application/src/lib/accounts-application.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AccountsController } from './accounts.controller';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'admin',
+      database: 'admin',
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AccountsController],
   providers: [],
   exports: [],
