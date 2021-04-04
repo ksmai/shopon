78b0cb0e9a519824c2109d5754f376af3555b6ab
ksmai <kasingmai@gmail.com>
2021-04-04 15:08:48 +0800

    test: set moduleNameMapper in jest.preset.js for all libs to use

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,13 @@
+const path = require('path');
 const nxPreset = require('@nrwl/jest/preset');
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)(\\?.*)?$': path.resolve(
+      __dirname,
+      '__mocks__',
+      'fileMock.js'
+    ),
+  },
+};
