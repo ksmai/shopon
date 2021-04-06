a64fb6c6bd08aaee5457ef801fcca4202571d640
ksmai <kasingmai@gmail.com>
2021-04-06 16:06:43 +0800

    feat: Account.create

--- libs/accounts/domain/src/lib/account.spec.ts
+++ libs/accounts/domain/src/lib/account.spec.ts
@@ -1,32 +1,63 @@
 import * as E from 'fp-ts/lib/Either';
 
-import { Account, CreateAccountData } from './account';
+import { Account, CreateAccountParams } from './account';
+import { hashPassword } from '@shopon/shared/domain';
 
 describe('Account', () => {
   describe('create', () => {
-    let data: CreateAccountData;
+    let data: CreateAccountParams;
 
-    beforeEach(() => {
+    beforeEach(async () => {
+      const password = 'Str0ngP@ssW0rd!';
+      const hashed = E.getOrElseW(fail)(await hashPassword(10)(password)());
       data = {
         name: 'Account Name',
         email: 'test@example.com',
-        password: 'secret-password',
+        password,
+        hashed,
       };
     });
 
     it('can be created with name', () => {
       const account = E.getOrElseW(fail)(Account.create(data));
-      expect(account.getName()).toBe(data.name);
+      expect(account.getName().getName()).toBe(data.name);
     });
 
     it('can be created with email', () => {
       const account = E.getOrElseW(fail)(Account.create(data));
-      expect(account.getEmail()).toBe(data.email);
+      expect(account.getEmail().getDisplayed()).toBe(data.email);
     });
 
-    it('can be created with password', () => {
+    it('can be created with password', async () => {
       const account = E.getOrElseW(fail)(Account.create(data));
-      expect(account.getPassword()).toBe(data.password);
+      const result = E.getOrElseW(fail)(
+        await account.getPassword().comparePassword(data.password)()
+      );
+      expect(result).toBe(true);
+    });
+
+    it('cannot be created with empty name', () => {
+      data.name = '';
+      const account = Account.create(data);
+      expect(E.isLeft(account)).toBe(true);
+    });
+
+    it('cannot be created with very very long name', () => {
+      data.name = 'LongName'.repeat(10000);
+      const account = Account.create(data);
+      expect(E.isLeft(account)).toBe(true);
+    });
+
+    it('cannot be created with invalid email', () => {
+      data.email = 'invalidEmail';
+      const account = Account.create(data);
+      expect(E.isLeft(account)).toBe(true);
+    });
+
+    it('cannot be created with weak password', () => {
+      data.password = '1';
+      const account = Account.create(data);
+      expect(E.isLeft(account)).toBe(true);
     });
   });
 });
--- libs/accounts/domain/src/lib/account.ts
+++ libs/accounts/domain/src/lib/account.ts
@@ -1,45 +1,77 @@
+import { pipe } from 'fp-ts/lib/function';
 import { Either } from 'fp-ts/lib/Either';
 import * as E from 'fp-ts/lib/Either';
+import { sequenceS } from 'fp-ts/lib/Apply';
 
-import { BaseError } from '@shopon/shared/error';
-import { DomainObject, Entity } from '@shopon/shared/domain';
+import {
+  DomainObject,
+  Entity,
+  Name,
+  Email,
+  Password,
+  CreateNameError,
+  CreateEmailError,
+  CreatePasswordError,
+} from '@shopon/shared/domain';
 
-export interface CreateAccountData {
+export interface CreateAccountParams {
   name: string;
   email: string;
   password: string;
+  hashed: string;
 }
+
+interface ConstructAccountParams {
+  name: Name;
+  email: Email;
+  password: Password;
+}
+
+export type CreateAccountError =
+  | CreateNameError
+  | CreateEmailError
+  | CreatePasswordError;
 
 @DomainObject()
 export class Account extends Entity {
-  private name: string;
-  private email: string;
-  private password: string;
+  private name: Name;
+  private email: Email;
+  private password: Password;
 
-  private constructor(name: string, email: string, password: string) {
+  private constructor(params: ConstructAccountParams) {
     super();
-    this.name = name;
-    this.email = email;
-    this.password = password;
+    this.name = params.name;
+    this.email = params.email;
+    this.password = params.password;
   }
 
-  public static create(data: CreateAccountData): Either<BaseError, Account> {
-    return E.right(new Account(data.name, data.email, data.password));
+  public static create(
+    params: CreateAccountParams
+  ): Either<CreateAccountError, Account> {
+    const properties = {
+      name: Name.create(params),
+      email: Email.create(params),
+      password: Password.create(params),
+    };
+    return pipe(
+      sequenceS(E.either)<CreateAccountError, typeof properties>(properties),
+      E.map((params) => new Account(params))
+    );
   }
 
   public getNamespaces(): string[] {
     return ['shopon', 'accounts'];
   }
 
-  public getName(): string {
+  public getName(): Name {
     return this.name;
   }
 
-  public getEmail(): string {
+  public getEmail(): Email {
     return this.email;
   }
 
-  public getPassword(): string {
+  public getPassword(): Password {
     return this.password;
   }
 }
