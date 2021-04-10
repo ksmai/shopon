0d097b580adac1bfc5b882221e30013b7a40c2f9
ksmai <kasingmai@gmail.com>
2021-04-10 15:41:15 +0800

    refactor: only use TypeOrmModule in accounts/db

--- libs/accounts/application/src/lib/accounts-application.module.ts
+++ libs/accounts/application/src/lib/accounts-application.module.ts
@@ -1,22 +1,11 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AccountsDomainModule } from '@shopon/accounts/domain';
+import { AccountsDbModule } from '@shopon/accounts/db';
 import { AccountsController } from './accounts.controller';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'admin',
-      password: 'admin',
-      database: 'admin',
-      autoLoadEntities: true,
-    }),
-  ],
+  imports: [AccountsDomainModule, AccountsDbModule],
   controllers: [AccountsController],
-  providers: [],
-  exports: [],
 })
 export class AccountsApplicationModule {}
--- libs/accounts/db/src/lib/accounts-db.module.ts
+++ libs/accounts/db/src/lib/accounts-db.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Account as AccountModel } from './account.model';
+import { AccountsRepository } from './accounts.repository';
 
 @Module({
-  providers: [],
-  exports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'admin',
+      database: 'admin',
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+    TypeOrmModule.forFeature([AccountModel]),
+  ],
+  providers: [AccountsRepository],
+  exports: [AccountsRepository],
 })
 export class AccountsDbModule {}
